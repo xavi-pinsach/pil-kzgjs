691b67bf947569961d655c73864aacdf65b15c30
Xavi Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2022-10-31 12:48:34 +0100

    Added kzg setup service

---
+++ src/kzg_setup.js
@@ -0,0 +1,111 @@
+/*
+    Copyright 2022 iden3
+
+    This file is part of pilkatejs
+
+    pilkatejs is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pilkatejs is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pilkatejs. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+const {readBinFile} = require("@iden3/binfileutils");
+const {readPTauHeader} = require("./powersoftau_utils");
+const {F1Field, BigBuffer} = require("ffjavascript");
+const {compile, newConstantPolsArray} = require("pilcom");
+const {log2} = require("pilcom/src/utils");
+const {stringifyBigInts} = require("ffjavascript").utils;
+const {Polynomial} = require("./polynomial/polynomial");
+const {F3G} = require("./f3g");
+
+
+module.exports.kzgSetup = async function (pilFile, pilConfigFile, cnstPolsFile, ptauFile, logger) {
+    logger.info("Starting kzg setup");
+
+    const {fd: fdPTau, sections: sectionsPTau} = await readBinFile(ptauFile, "ptau", 1, 1 << 22, 1 << 24);
+    if (!sectionsPTau[2] || !sectionsPTau[3]) {
+        if (logger) logger.error("Powers of tau is not prepared.");
+        return -1;
+    }
+
+    const {curve, power: ptauPower} = await readPTauHeader(fdPTau, sectionsPTau);
+    const F = new F1Field("0xFFFFFFFF00000001");
+    const Fr = curve.Fr;
+    const G1 = curve.G1;
+    const G2 = curve.G2;
+
+    // PIL compile
+    const pil = await compile(F, pilFile, null, pilConfigFile);
+
+    //Find the max PIL polynomial degree
+    let maxPilPolDeg = 0;
+    for (const polRef in pil.references) {
+        maxPilPolDeg = Math.max(maxPilPolDeg, pil.references[polRef].polDeg);
+    }
+
+    const pilPower = log2(maxPilPolDeg - 1) + 1;
+    const domainSize = 2 ** pilPower;
+
+    if (pilPower > ptauPower) {
+        if (logger) logger.error(`PIL polynomials degree is too big for this powers of Tau, 2**${pilPower} > 2**${ptauPower}`);
+        return -1;
+    }
+
+    const sizeG1 = G1.F.n8 * 2;
+    const sizeG2 = G2.F.n8 * 2;
+
+    const pTauBuffer = new BigBuffer(domainSize * sizeG1);
+    await fdPTau.readToBuffer(pTauBuffer, 0, domainSize * sizeG1, sectionsPTau[2][0].p);
+
+    let preprocessed = {
+        protocol: "kate",
+        curve: "bn128",
+        power: pilPower,
+        // w: Fr.toObject(Fr.w[pilPower]),
+        S_2: G2.toObject(await fdPTau.read(sizeG2, sectionsPTau[3][0].p + sizeG2)),
+        polynomials: {},
+    };
+
+    // Load preprocessed polynomials
+    const cnstPols = newConstantPolsArray(pil);
+    await cnstPols.loadFromFile(cnstPolsFile);
+
+    for (let i = 0; i < cnstPols.$$nPols; i++) {
+        const cnstPol = cnstPols.$$defArray[i];
+        const cnstPolBuffer = cnstPols.$$array[i];
+
+        if (logger) {
+            logger.info(`Preparing ${cnstPol.name} polynomial`);
+        }
+
+        // Convert from one filed to another (bigger), TODO check if a new constraint is needed
+        let polEvalBuff = new BigBuffer(cnstPolBuffer.length * Fr.n8);
+        for (let i = 0; i < cnstPolBuffer.length; i++) {
+            polEvalBuff.set(Fr.e(cnstPolBuffer[i]), i * Fr.n8);
+        }
+
+        let pol = await Polynomial.fromBuffer(polEvalBuff, Fr, logger);
+
+        // Calculates the commitment
+        const polCommitment = await pol.evaluateG1(pTauBuffer, curve, logger);
+
+        // Add the commitment to the preprocessed polynomials
+        preprocessed.polynomials[cnstPol.name] = G1.toObject(polCommitment);
+    }
+
+    // Finish curve & close file descriptors
+    await curve.terminate();
+    fdPTau.close();
+
+    logger.info("Kate setup finished");
+
+    return stringifyBigInts(preprocessed);
+}
--- src/services/kzg_setup_srv.js
+++ src/services/kzg_setup_srv.js
@@ -1,4 +1,6 @@
 const Logger = require("logplease");
+const {kzgSetup} = require("../kzg_setup");
+const bfj = require("bfj");
 
 const logger = Logger.create("pil-kzgjs", {showTimestamp: false});
 Logger.setLogLevel("INFO");
@@ -6,9 +8,9 @@ Logger.setLogLevel("INFO");
 module.exports.kzgSetupService = async function (pilFile, pilConfigFile, cnstPolsFile, ptauFile, preprocessedFile, options) {
     if (options.verbose) Logger.setLogLevel("DEBUG");
 
-    //const vkOutput = await kzg.kzgSetup(pilFile, pilConfigFile, cnstPolsFile, ptauFile, logger);
+    const vkOutput = await kzgSetup(pilFile, pilConfigFile, cnstPolsFile, ptauFile, logger);
 
-    //await bfj.write(preprocessedFile, vkOutput, {space: 1});
+    await bfj.write(preprocessedFile, vkOutput, {space: 1});
 
     return 0;
 }
