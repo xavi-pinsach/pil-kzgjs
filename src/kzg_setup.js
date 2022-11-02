22fc4e8f2586894db6a74159e3bbd10b2def3261
Xavi Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2022-11-02 10:35:27 +0100

    typos

--- src/kzg_setup.js
+++ src/kzg_setup.js
@@ -1,20 +1,20 @@
 /*
     Copyright 2022 iden3
 
-    This file is part of pilkatejs
+    This file is part of pil-kzgjs
 
-    pilkatejs is a free software: you can redistribute it and/or modify it
+    pil-kzgjs is a free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    pilkatejs is distributed in the hope that it will be useful, but WITHOUT
+    pil-kzgjs is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
     License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with pilkatejs. If not, see <https://www.gnu.org/licenses/>.
+    along with pil-kzgjs. If not, see <https://www.gnu.org/licenses/>.
 */
 
 const {readBinFile} = require("@iden3/binfileutils");
